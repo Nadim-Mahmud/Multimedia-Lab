e6f250dd62138481dc6bf4c6f7765d14951d787d
Nadim-Mahmud <mahmudnadim16@gmail.com>
2022-01-04 09:37:50 +0600

    run length optimied using char 256

--- run_length_optimized.cpp
+++ run_length_optimized.cpp
@@ -2,8 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-char str[1000000];
-string en = "", decr = "";
+string en = "", decr = "",str;
 
 string to_string(int cn){
     string tmp = "";
@@ -15,52 +14,81 @@ string to_string(int cn){
     return tmp;
 }
 
+void write(string text, char input_f[] = "binary.encd" , bool check = true){
+
+    //string text = "Nadim";
+
+    ofstream outfile(input_f, ofstream::binary);
+    unsigned int size1 = text.size();
+    outfile.write(reinterpret_cast<char *>(&size1), sizeof(size1)  );
+    outfile.write( text.c_str(), text.size() );
+    outfile.close();
+}
+
+
+
+string read(char input_f[] = "binary.encd", bool check = true){
+
+    ifstream infile(input_f, ifstream::binary);
+
+    unsigned int size1 = 0;
+    infile.read(reinterpret_cast<char *>(&size1), sizeof(size1)  );
+
+    string buffer;
+    buffer.resize(size1);
+
+    infile.read(&buffer[0],  buffer.size() );
+    infile.close();
+
+    //cout << "buffer = \n" << buffer << '\n';
+    return buffer;
+}
+
 int main(){
 
     int cn = 0, i, j;
+    char ch;
 
     ifstream run_in;
-    ofstream run_out,re_dec;
+    ofstream re_dec;
 
     run_in.open("run_in.txt");
-    run_out.open("run_out.txt");
+    //run_in.open("run.png");
+    //re_dec.open("run2.png");
     re_dec.open("re_dec.txt");
 
     run_in>>str;
 
-    for(i = 0; str[i]; i++){
+    for(i = 0; i<str.size(); i++){
         cn = 0;
-        for(j = i; str[j] && str[i]==str[j]; j++){
+        ch = 0;
+        for(j = i; j<str.size() && str[i]==str[j] && (cn+1) < 256; j++){
             cn++;
+            ch++;
         }
         i = j - 1;
         en += str[i];
-        //cout<<to_string(cn)<<endl;
-        en += to_string(cn);
+        en += ch;
     }
 
-    run_out<<"Encrypted text : "<<en<<endl;
+    //cout<<"Encrypted text : "<<en<<endl;
+
+    write(en);
+    en = read();
 
     // decryption section
-
     for(i = 0; en[i]; i++){
-        if(!isdigit(en[i])){
-
-            int cnt = 0;
-            for(j = i+1; en[j] && isdigit(en[j]); j++){
-                cnt = cnt*10 + (int)(en[j] - '0');
-            }
-            for(j = 0; j < cnt; j++){
+        if(isalpha(en[i])){
+            cn = (int)en[i+1];
+            for(j = 0; j < cn; j++){
                 decr += en[i];
             }
+            i++;
         }
     }
 
-    re_dec<<"After decryption : "<<decr<<endl;
-
-    run_in.close();
-    run_out.close();
-    re_dec.close();
+    cout<<"After decryption : "<<decr<<endl;
+    re_dec<<decr;
 
     return 0;
 }
